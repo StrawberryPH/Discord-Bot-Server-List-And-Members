07dadf6058285e50fd7728b2f3cebdbee86bff99
Strawberry <119730145+StrawberryPH@users.noreply.github.com>
2024-08-26 11:08:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import discord
 
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=prefix, help_command=None, intents=discord.Intents.all())
 
 TOKEN = input("Please enter your bot token:")
 
 
-client = discord.Client(intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
     print("servers")
     for guild in client.guilds:
         print(f"- {guild.name} ({guild.id}), {guild.member_count} members")
 
-client.run(TOKEN)
+bot.run(TOKEN)

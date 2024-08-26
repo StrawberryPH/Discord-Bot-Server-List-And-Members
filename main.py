8fd79b99eb4238408fafb4f5322093878639723e
Strawberry <119730145+StrawberryPH@users.noreply.github.com>
2024-08-26 11:10:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 intents = discord.Intents.all()
-bot = commands.Bot( help_command=None, intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=".", help_command=None, intents=discord.Intents.all())
 
 TOKEN = input("Please enter your bot token:")
 
